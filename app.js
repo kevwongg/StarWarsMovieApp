fbc3c3a68643bf017038fcccfaad621b76e7427a
kevinw123 <kevinwong_895@hotmail.com>
2016-08-26 13:56:39 -0700

    Adding ejs for views

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 var express = require('express');
 var app = express();
-
+app.set('view engine', 'ejs');
 //Routes
 
 //Home
 app.get('/', function(req, res){
-	res.send("This is a server response on home page");
+	res.render('home');
 });
 
+// movie single
+app.get('/star_wars_episode/:episode_number?', function(req, res){
+	var episode_number = req.params.episode_number;
+	res.send("This is the page for episode " + episode_number);
+});
+
+// notFound
+app.get('*', function(req, res){
+	res.send("This is not the page that you're looking for");
+});
 app.listen(3000, function(){
 	console.log("Applictaion running on local host 3000")
 });
