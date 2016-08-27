510f48014826cf40a0f7f06267d0da8891d1c654
kevinw123 <kevinwong_895@hotmail.com>
2016-08-26 22:28:13 -0700

    Adding single movies page

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,34 @@
 var moviesJSON = require('../movies.json');
 //Home
-exports.home = function(req, res){
-	var movies = moviesJSON.movies;
-	res.render('home', {
-		title : "Star Wars Movies",
-		movies: movies
-	});
+exports.home = function(req, res) {
+    var movies = moviesJSON.movies;
+    res.render('home', {
+        title: "Star Wars Movies",
+        movies: movies
+    });
 };
 
 // movie single
-exports.movie_single =function(req, res){
-	var episode_number = req.params.episode_number;
-	res.send("This is the page for episode " + episode_number);
+exports.movie_single = function(req, res) {
+    var episode_number = req.params.episode_number;
+    var movies = moviesJSON.movies;
+    if (episode_number >= 1 && episode_number <= 6) {
+        var movie = movies[episode_number - 1];
+        var title = movie.title;
+        var main_characters = movie.main_characters;
+        res.render('movie_single', {
+            movies: movies,
+            title: title,
+            movie: movie,
+            main_characters : main_characters
+        });
+    } else {
+        res.send("This is not the page you're looking for");
+    }
+
 };
 
 // notFound
-exports.notFound = function(req, res){
-	res.send("This is not the page that you're looking for");
+exports.notFound = function(req, res) {
+    res.send("This is not the page that you're looking for");
 };
